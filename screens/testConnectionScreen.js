0b46913d9d882770b2850168b04dc6c9f8067310
MrBoombastic <mr.tolek2003@gmail.com>
2021-07-11 17:56:55 +0200

    4

--- screens/testConnectionScreen.js
+++ screens/testConnectionScreen.js
@@ -9,7 +9,7 @@ const getIP = () => {
     return new Promise((resolve, reject) => {
         exec('ip -6 route | grep -m1 "he-ipv6 proto"', (err, stdout) => {
             if (err) reject(false);
-            resolve(stdout.split("dev")[0].slice(0, -3));
+            resolve(stdout.split("dev")[0].slice(0, -4));
         });
     });
 };
