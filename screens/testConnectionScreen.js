b67f9b4f8a7ff247b71dba2fac6379f5ac576a7d
MrBoombastic <mr.tolek2003@gmail.com>
2021-07-14 12:00:29 +0200

    testConnectionScreen.js - additional tests, ip autofill from answer file

--- screens/testConnectionScreen.js
+++ screens/testConnectionScreen.js
@@ -1,10 +1,11 @@
 const styles = require("../styles"),
     {appendList, printTestSummary, request6, validateIP} = require("../tools"),
-    {list, prompt} = require('blessed');
+    {list, prompt} = require('blessed'),
+    fs = require("fs");
 
 module.exports = async (screen) => {
     const testList = list(styles.list),
-        tests = {count: 2, passed: 0};
+        tests = {count: 4, passed: 0};
 
     testList.focus();
     screen.append(testList);
@@ -13,7 +14,13 @@ module.exports = async (screen) => {
         if (data.content === "1.  Exit") return require("./welcomeScreen")(screen);
     });
 
-    prompt(styles.prompt(screen)).input('What IPv6 from your assigned block do you want to test?', '', async function (err, IP) {
+    let defaultIP = "";
+    if (fs.existsSync("./answer.json")) {
+        const answerFile = JSON.parse(fs.readFileSync("./answer.json", "UTF-8"));
+        if (answerFile?.Routed) defaultIP = answerFile.Routed.split("/")?.[0];
+    }
+    prompt(styles.prompt(screen)).input('What IPv6 from your assigned block do you want to test?', defaultIP, async function (err, IP) {
+
         //stage1
         IP = validateIP(IP);
         testList.addItem("INFO: testing started...");
@@ -28,21 +35,40 @@ module.exports = async (screen) => {
         if (!IP) {
             appendList(screen, testList, "INFO: No valid IP found, skipping test.");
             return printTestSummary(screen, testList, tests);
+        }
+        appendList(screen, testList, `TEST: sending request to Ipify from ${IP}`);
+        const reqIpify = await request6(IP, "https://api64.ipify.org/?format=json");
+        if (reqIpify.statusCode === 200) {
+            retrievedIP = JSON.parse(reqIpify.body).ip;
+            appendList(screen, testList, `RESPONSE: ${retrievedIP} - PASSED!`);
+            tests.passed++;
         } else {
-            appendList(screen, testList, `TEST: sending request to Ipify from ${IP}`);
-            const req = await request6(IP, "https://api64.ipify.org/?format=json");
-            if (req.statusCode === 200) {
-                retrievedIP = JSON.parse(req.body).ip;
-                appendList(screen, testList, `RESPONSE: ${retrievedIP} - PASSED!`);
-                tests.passed++;
-            } else {
-                appendList(screen, testList, `RESPONSE: not OK (${req?.statusText || req?.code || JSON.stringify(req)}) - FAILED!`);
-                return printTestSummary(screen, testList, tests);
-            }
+            appendList(screen, testList, `RESPONSE: not OK (${reqIpify?.statusText || reqIpify?.code || JSON.stringify(reqIpify)}) - FAILED!`);
+            return printTestSummary(screen, testList, tests);
         }
 
+
         //stage 3
-        //todo: youtube and fb ping test
+        appendList(screen, testList, `TEST: sending request to Google`);
+        const reqGoogle = await request6(IP, "https://google.com");
+        if (reqGoogle.statusCode === 200) {
+            appendList(screen, testList, `RESPONSE: OK - PASSED!`);
+            tests.passed++;
+        } else {
+            appendList(screen, testList, `RESPONSE: not OK (${reqGoogle?.statusText || reqGoogle?.code || JSON.stringify(reqGoogle)}) - FAILED!`);
+            return printTestSummary(screen, testList, tests);
+        }
+
+        //stage 4
+        appendList(screen, testList, `TEST: sending request to Facebook`);
+        const reqFB = await request6(IP, "https://facebook.com");
+        if (reqGoogle.statusCode === 200) {
+            appendList(screen, testList, `RESPONSE: OK - PASSED!`);
+            tests.passed++;
+        } else {
+            appendList(screen, testList, `RESPONSE: not OK (${reqFB?.statusText || reqFB?.code || JSON.stringify(reqFB)}) - FAILED!`);
+            return printTestSummary(screen, testList, tests);
+        }
 
 
         //summary
